453052c4c17cac049bc9cec41d567241eae9047e
Dinesh <aakala420@gmail.com>
2020-12-05 10:19:58 -0500

    project structure updated

---
+++ app/src/main/java/com/example/shoponline/ProductsActivity.java
@@ -0,0 +1,15 @@
+package com.example.shoponline;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
