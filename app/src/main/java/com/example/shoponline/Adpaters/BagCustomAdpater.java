32915fa4ada781888ef4d4fb7b4a4b43bda8607e
DineshReddy420 <64855777+DineshReddy420@users.noreply.github.com>
2020-12-05 19:32:10 -0500

    updating bagcustom_adapter
    finding the database document on firebase

--- app/src/main/java/com/example/shoponline/Adpaters/BagCustomAdpater.java
+++ app/src/main/java/com/example/shoponline/Adpaters/BagCustomAdpater.java
@@ -1,28 +1,73 @@
 package com.example.shoponline.Adpaters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.shoponline.Models.Products;
+import com.example.shoponline.R;
+
+import java.util.ArrayList;
 
 public class BagCustomAdpater extends BaseAdapter {
+    ArrayList<Products> product;
 
+    public BagCustomAdpater(ArrayList<Products> product) {
+        this.product=product;
+
+    }
     @Override
     public int getCount() {
-        return 0;
+        return product.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Products getItem(int position) {
+        return product.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        BagCustomAdpater.ViewHolder viewHolder;
+        if(convertView==null){
+            convertView= LayoutInflater.from(parent.getContext()).inflate(R.layout.bag_layout, null);
+            viewHolder=new BagCustomAdpater.ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+
+        }
+        else
+        {
+            viewHolder=(BagCustomAdpater.ViewHolder) convertView.getTag();
+        }
+
+        Products products=getItem(position);
+        viewHolder.BrandName.setText(products.getBrand_name());
+        viewHolder.ProductName.setText(products.getProduct_name());
+        viewHolder.Price.setText(products.getProduct_price());
+        viewHolder.ImageTV.setImageResource(products.getImage_file());
+        return convertView;
+    }
+
+    class ViewHolder{
+
+        TextView BrandName;
+        TextView ProductName;
+        TextView Price;
+        ImageView ImageTV;
+        public  ViewHolder(View view)
+        {
+            BrandName=view.findViewById(R.id.Brand);
+            ProductName=view.findViewById(R.id.Product_name);
+            Price=view.findViewById(R.id.Price);
+            ImageTV=view.findViewById(R.id.image_view);
+        }
     }
 }
